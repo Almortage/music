35a64900682dbd682ecff055b224af72cb7d7b3c
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-04-07 21:45:57 +0200

    Update thumbnails.py

--- FallenMusic/Helpers/thumbnails.py
+++ FallenMusic/Helpers/thumbnails.py
@@ -31,7 +31,7 @@ from PIL import Image, ImageChops, ImageDraw, ImageEnhance, ImageFilter, ImageFo
 from youtubesearchpython.__future__ import VideosSearch
 
 from config import FAILED
-from FallenMusic import BOT_ID, LOGGER, app
+from AlmortagelMusic import BOT_ID, LOGGER, app
 
 
 def changeImageSize(maxWidth, maxHeight, image):
@@ -108,7 +108,7 @@ async def gen_thumb(videoid, user_id):
         x = f.resize((107, 107))
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"FallenMusic/Helpers/utils/circle.png")
+        bg = Image.open(f"AlmortagelMusic/Helpers/utils/circle.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
@@ -143,10 +143,10 @@ async def gen_thumb(videoid, user_id):
         background.paste(image3, (0, 0), mask=image3)
 
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("FallenMusic/Helpers/utils/font2.ttf", 45)
-        ImageFont.truetype("FallenMusic/Helpers/utils/font2.ttf", 70)
-        arial = ImageFont.truetype("FallenMusic/Helpers/utils/font2.ttf", 30)
-        ImageFont.truetype("FallenMusic/Helpers/utils/font.ttf", 30)
+        font = ImageFont.truetype("AlmortagelMusic/Helpers/utils/font2.ttf", 45)
+        ImageFont.truetype("AlmortagelMusic/Helpers/utils/font2.ttf", 70)
+        arial = ImageFont.truetype("AlmortagelMusic/Helpers/utils/font2.ttf", 30)
+        ImageFont.truetype("AlmortagelMusic/Helpers/utils/font.ttf", 30)
         para = textwrap.wrap(title, width=32)
         try:
             draw.text(
@@ -253,7 +253,7 @@ async def gen_qthumb(videoid, user_id):
         x = f.resize((107, 107))
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"FallenMusic/Helpers/utils/circle.png")
+        bg = Image.open(f"AlmortagelMusic/Helpers/utils/circle.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
@@ -288,10 +288,10 @@ async def gen_qthumb(videoid, user_id):
         background.paste(image3, (0, 0), mask=image3)
 
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("FallenMusic/Helpers/utils/font2.ttf", 45)
-        ImageFont.truetype("FallenMusic/Helpers/utils/font2.ttf", 70)
-        arial = ImageFont.truetype("FallenMusic/Helpers/utils/font2.ttf", 30)
-        ImageFont.truetype("FallenMusic/Helpers/utils/font.ttf", 30)
+        font = ImageFont.truetype("AlmortagelMusic/Helpers/utils/font2.ttf", 45)
+        ImageFont.truetype("AlmortagelMusic/Helpers/utils/font2.ttf", 70)
+        arial = ImageFont.truetype("AlmortagelMusic/Helpers/utils/font2.ttf", 30)
+        ImageFont.truetype("AlmortagelMusic/Helpers/utils/font.ttf", 30)
         para = textwrap.wrap(title, width=32)
         try:
             draw.text(
