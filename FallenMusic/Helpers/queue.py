583a23e56c515d128e1cdeef14c8d5a1cfab8f9d
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-04-07 21:46:41 +0200

    Update queue.py

--- FallenMusic/Helpers/queue.py
+++ FallenMusic/Helpers/queue.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from FallenMusic import fallendb
+from AlmortagelMusic import fallendb
 
 
 async def put(
